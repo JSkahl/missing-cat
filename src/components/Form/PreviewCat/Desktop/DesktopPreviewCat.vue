dc71e015081846132f6dbd0a0ef971472f5cc543
JSkahl <josekahl333@gmail.com>
2025-03-12 20:25:26 -0300

    feat(form): Creating tree strucutre
    Creating default tree structure to be followed along the project

---
+++ src/components/Form/PreviewCat/Desktop/DesktopPreviewCat.vue
@@ -0,0 +1,11 @@
+<script setup>
+  
+</script>
+
+<template>
+  <h1>Preview Cat (Desktop)</h1>
+</template>
+
+<style scoped>
+  
+</style>
---
+++ src/components/Form/PreviewCat/Mobile/MobilePreviewCat.vue
@@ -0,0 +1,11 @@
+<script setup>
+  
+</script>
+
+<template>
+  <h1>Preview Cat (Mobile)</h1>
+</template>
+
+<style scoped>
+  
+</style>
