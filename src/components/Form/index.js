def8359a165a9450ab92ee62bbd37a341e3e2738
JSkahl <josekahl333@gmail.com>
2025-03-13 16:23:58 -0300

    feat(form): Creating Form component index
    Creating index.js to all Form's components to a better organization

---
+++ src/components/Form/index.js
@@ -0,0 +1,16 @@
+import MobilePreviewCat from "Mobile/MobilePreviewCat";
+import MobileInfoCat from "Mobile/MobileInfoCat";
+import MobileContactOwner from "Mobile/MobileContactOwner";
+
+import DesktopPreviewCat from "Desktop/DesktopPreviewCat";
+import DesktopInfoCat from "Desktop/DesktopInfoCat";
+import DesktopContactOwner from "Desktop/DesktopContactOwner";
+
+export {
+  MobilePreviewCat,
+  MobileInfoCat,
+  MobileContactOwner,
+  DesktopPreviewCat,
+  DesktopInfoCat,
+  DesktopContactOwner,
+}
