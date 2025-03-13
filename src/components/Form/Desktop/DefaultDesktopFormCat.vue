bc8cce7f32321955b5e0ffeeece869d22f28902b
JSkahl <josekahl333@gmail.com>
2025-03-13 20:36:39 -0300

    chore(form): Finishing Form Cat component's structure
    Finalizing the whole structure to the Cat Formulary component by making
    it future proof with both desktop and mobile components

---
+++ src/components/Form/Desktop/DefaultDesktopFormCat.vue
@@ -0,0 +1,11 @@
+<script setup>
+  
+</script>
+
+<template>
+  <h1>Default Form Cat (Desktop)</h1>
+</template>
+
+<style scoped>
+  
+</style>
---
+++ src/components/Form/Mobile/DefaultMobileFormCat.vue
@@ -0,0 +1,11 @@
+<script setup>
+  
+</script>
+
+<template>
+  <h1>Default Form Cat (Mobile)</h1>
+</template>
+
+<style scoped>
+  
+</style>
