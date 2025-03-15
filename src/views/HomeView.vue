c37376867649ada2e4a4b60189665b746bea167c
JSkahl <josekahl333@gmail.com>
2025-03-15 12:22:22 -0300

    feat(home): created home structure
    Created HomeView structure and changed views names to the pattern

---
+++ src/views/HomeView.vue
@@ -0,0 +1,44 @@
+<script setup>
+import Cat from "icons/Cat.vue";
+</script>
+
+<template>
+  <div class="container">
+    <div class="header">
+      <Cat class="cat-icon" />
+      <h2>Achou a minha gata?</h2>
+    </div>
+    <p>Por favor, confira se é uma das duas e entre em contato comigo</p>
+    <div class="cats">
+      <div class="astrid">
+        <div class="picture"></div>
+        <h4>Astrid</h4>
+      </div>
+      <div class="mia">
+        <div class="picture"></div>
+        <h4>Mia</h4>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  width: 55%;
+  height: 95vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: red;
+}
+
+.cat-icon {
+  color: var(--black);
+  background-color: green;
+}
+
+:deep(.material-design-icon__svg) {
+  width: 10em;
+  height: 10em;
+}
+</style>
