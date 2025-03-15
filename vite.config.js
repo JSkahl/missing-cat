840f961d840fe9bfe0a0a599c1972537efe96973
JSkahl <josekahl333@gmail.com>
2025-03-15 05:43:15 -0300

    chore(vite): created aliases
    Created aliases to src and vue-material-design-icons

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      icons: fileURLToPath(
+        new URL("./node_modules/vue-material-design-icons", import.meta.url),
+      ),
+    },
+  },
+});
