12263e129631e7179c42b3454be985931a63c902
JSkahl <josekahl333@gmail.com>
2025-03-16 06:13:02 -0300

    feat(data): created static data
    Created static data of the cats and the owner's contact to use in the
    components

---
+++ src/data/cats.js
@@ -0,0 +1,14 @@
+export const cats = {
+  astrid: {
+    name: "astrid",
+    age: 2,
+    sex: "fem",
+    color: "preta",
+  },
+  mia: {
+    name: "mia",
+    age: 2,
+    sex: "fem",
+    color: "frajola",
+  }
+}
---
+++ src/data/contact.js
@@ -0,0 +1,5 @@
+export const contact = {
+  name: "José",
+  number: "(47) 99923-9568",
+  email: "josekahl333@gmail.com",
+}
