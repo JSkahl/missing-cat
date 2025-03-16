4ffada889774d36b5992ba0af19703c6fbf19c32
JSkahl <josekahl333@gmail.com>
2025-03-16 15:18:11 -0300

    chore(data): added picture as a static data

--- src/data/cats.js
+++ src/data/cats.js
@@ -1,11 +1,13 @@
 export const cats = {
   astrid: {
+    picture: new URL('@/assets/img/astrid.png', import.meta.url).href,
     name: "astrid",
     age: 2,
     sex: "fem",
     color: "preta",
   },
   mia: {
+    picture: new URL('@/assets/img/mia.png', import.meta.url).href,
     name: "mia",
     age: 2,
     sex: "fem",
