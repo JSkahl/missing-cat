3db5f30e4a909c9dc1dec454a93b1363b77af50b
JSkahl <josekahl333@gmail.com>
2025-03-26 20:53:45 -0300

    chore(home): added home components to DefaultHome

--- src/components/Home/DefaultHome.vue
+++ src/components/Home/DefaultHome.vue
@@ -1,9 +1,14 @@
 <script setup>
-  import DesktopHome from './index.js';
-  import MobileHome from './index.js';
+import DesktopHome from "./index.js";
+import MobileHome from "./index.js";
+
+import { useScreen } from "@/composables/index.js";
+const { isMobile } = useScreen();
 </script>
 
 <template>
+  <DesktopHome v-if="!isMobile"/>
+  <MobileHome v-else/>
 </template>
 
 <style scoped></style>
