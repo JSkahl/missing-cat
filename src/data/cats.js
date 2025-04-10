432e06aaca8a1bdac884265d9b9d7b7805784fe4
JSkahl <josekahl333@gmail.com>
2025-04-10 09:43:54 -0300

    chore(data): add description to static data

--- src/data/cats.js
+++ src/data/cats.js
@@ -2,6 +2,7 @@ export const cats = {
   astrid: {
     picture: new URL('@/assets/img/astrid.png', import.meta.url).href,
     name: "astrid",
+    description: "A Astrid é uma gata preta de olhos castanhos e uma mancha branca na pata traseira esquerda. Ela é arisca e desconfiada, então ao se aproximar dela tenha cuidado para não espanta-la.",
     age: 2,
     sex: "fem",
     color: "preta",
@@ -9,6 +10,7 @@ export const cats = {
   mia: {
     picture: new URL('@/assets/img/mia.png', import.meta.url).href,
     name: "mia",
+    description: "A Mia é uma gata frajola de olhos castanhos muito miona. Ela adora carinho e vai ficar miando sem parar se não receber carinho logo. Ela é arisca e desconfiada, então ao se aproximar dela tenha cuidado para não espanta-la.",
     age: 2,
     sex: "fem",
     color: "frajola",
